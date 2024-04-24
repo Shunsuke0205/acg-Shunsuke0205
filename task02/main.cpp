ccf2d8cfaddf811a87d256dd7865e3fb241ac818
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2024-04-24 21:32:07 +0900

    impl: intersections against curve

--- task02/main.cpp
+++ task02/main.cpp
@@ -40,14 +40,18 @@ int number_of_intersection_ray_against_edge(
     const Eigen::Vector2f &pe) {
   auto a = area(org, org + dir, ps);
   auto b = area(org, pe, org + dir);
-  auto c = area(org, ps, pe);
-  auto d = area(dir+ps, ps, pe);
-  if (a * b > 0.f && d * c < 0.f) { return 1; }
-  return 0;
+  auto c = area(org, pe, ps);
+  auto d = area(org + dir, ps, pe);
+  if (a * b >= 0.f && d * c >= 0.f) {
+    return 1;
+  }
   // the following code was a bug
-  //auto d = area(org + dir, ps, pe);
+  // auto d = area(org + dir, ps, pe);
   //if (a * b > 0.f && d * c > 0.f && fabs(d) > fabs(c)) { return 1; }
+  return 0;
 }
+
+
 
 /***
  *
@@ -65,8 +69,62 @@ int number_of_intersection_ray_against_quadratic_bezier(
     const Eigen::Vector2f &pc,
     const Eigen::Vector2f &pe) {
   // comment out below to do the assignment
-  return number_of_intersection_ray_against_edge(org, dir, ps, pe);
-  // write some code below to find the intersection between ray and the quadratic
+  const Eigen::Vector2f dir_perpendicular(dir[1], -dir[0]); // 90 degree rotation
+
+  std::vector<float> point_on_curve_x_coordinate_polynominal = {0.0, 0.0, 0.0};
+  std::vector<float> point_on_curve_y_coordinate_polynominal = {0.0, 0.0, 0.0};
+
+  // p(t) = (1 - t)^2 * ps + 2 * (1 - t) * t * pc + t^2 * pe
+  point_on_curve_x_coordinate_polynominal[0] = ps[0]; // constant term
+  point_on_curve_x_coordinate_polynominal[1] = 2 * (pc[0] - ps[0]); // linear term
+  point_on_curve_x_coordinate_polynominal[2] = ps[0] - 2 * pc[0] + pe[0]; // quadratic term
+  point_on_curve_y_coordinate_polynominal[0] = ps[1]; // constant term
+  point_on_curve_y_coordinate_polynominal[1] = 2 * (pc[1] - ps[1]); // linear term
+  point_on_curve_y_coordinate_polynominal[2] = ps[1] - 2 * pc[1] + pe[1]; // quadratic term
+
+  // shift the origin
+  point_on_curve_x_coordinate_polynominal[0] -= org[0];
+  point_on_curve_y_coordinate_polynominal[0] -= org[1];
+
+  // product of dir_perpendicular and p(t)
+  std::vector<float> dir_perpendicular_dot_point_on_curve = {0.0, 0.0, 0.0};
+  for (int i = 0; i < 3; i++) {
+    dir_perpendicular_dot_point_on_curve[i] = point_on_curve_x_coordinate_polynominal[i] * dir_perpendicular[0] +
+                                              point_on_curve_y_coordinate_polynominal[i] * dir_perpendicular[1];
+  }
+  
+  // f(t) = a * t^2 + b * t + c
+  float a = dir_perpendicular_dot_point_on_curve[2];
+  float b = dir_perpendicular_dot_point_on_curve[1];
+  float c = dir_perpendicular_dot_point_on_curve[0];
+
+  float epsilon = 1e-6;
+  if (abs(a) < epsilon) { // in case f(t) is linear
+    if ((b + c) * c <= epsilon) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  float root_minus = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+  float root_plus  = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+  
+  int number_of_roots = 0;
+  if (0 <= root_minus && root_minus <= 1) {
+    Eigen::Vector2f point_on_curve = (1 - root_minus) * (1 - root_minus) * ps + 2 * (1 - root_minus) * root_minus * pc + root_minus * root_minus * pe;
+    if (area(org, org + dir_perpendicular, point_on_curve) < 0) {
+      number_of_roots += 1;
+    }
+  }
+  if (0 <= root_plus && root_plus <= 1) {
+    Eigen::Vector2f point_on_curve = (1 - root_plus) * (1 - root_plus) * ps + 2 * (1 - root_plus) * root_plus * pc + root_plus * root_plus * pe;
+    if (area(org, org + dir_perpendicular, point_on_curve) < 0) {
+      number_of_roots += 1;
+    }
+  }
+
+  return number_of_roots;
 }
 
 int main() {
@@ -83,7 +141,7 @@ int main() {
   for (unsigned int ih = 0; ih < height; ++ih) {
     for (unsigned int iw = 0; iw < width; ++iw) {
       const auto org = Eigen::Vector2f(iw + 0.5, ih + 0.5); // pixel center
-      const auto dir = Eigen::Vector2f(60., 20.); // search direction
+      const auto dir = Eigen::Vector2f(6000., 2000.); // search direction
       int count_cross = 0;
       for (const auto &loop: loops) { // loop over loop (letter R have internal/external loops)
         for (const auto &edge: loop) { // loop over edge in the loop
